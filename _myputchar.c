fa7640ecfa4dadda64e7358c2b1649c9d925e964
Donwafula <donwafula4@gmail.com>
2023-08-22 04:32:51 -0700

    Updated _myputchar to normal

--- _myputchar.c
+++ _myputchar.c
@@ -1,36 +1,13 @@
-#include <unistd.h>
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * _myputchar - prints character value represented by c
- * @c: int value representing the character to print
- * Return: 0 on success or -1 for an error
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _myputchar(int c)
+int _putchar(char c)
 {
-	return (write_buffer(c));
-}
-
-/**
- * write_buffer - saves character in a buffer
- * @c: char to save to the buffer
- * Return: 1
- */
-
-int write_buffer(char c)
-{
-	static char buffering[1024];
-	static int a;
-
-	if (c == -1 || a == 1024)
-	{
-		write(1, buffering, a);
-		a = 0;
-	}
-	if (c != -1)
-	{
-		buffering[a++] = c;
-	}
-	return (1);
+	return (write(1, &c, 1));
 }
