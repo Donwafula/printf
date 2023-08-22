eb8ce38bfc71bf7ede5966def3aa6ec6ead33930
Donwafula <donwafula4@gmail.com>
2023-08-22 03:47:20 -0700

    Updated _myputchar to include buffering

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 int _myputchar(int c);
+int write_buffer(char c);
 int _printf(const char *format, ...);
 
 #endif 
