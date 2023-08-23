c426524c8e7f45f9c4fcea08d2c4afe0291b5797
donwafula <107577767+Donwafula@users.noreply.github.com>
2023-08-23 05:39:25 +0300

    Update 1_Educ_is_when.c

--- 1_Educ_is_when.c
+++ 1_Educ_is_when.c
@@ -1,98 +1,18 @@
-#include <stdarg.h>
 #include "main.h"
 
-/* Helper function to print a character */
-int _myputchar(char c);
-
 /**
- * _printf - Custom printf function
- * @format: Format string with directives
- *
- * Return: Number of characters printed
- */
-int _printf(const char *format, ...)
+ * handle_int - printa an int base 10 to stdout
+ * @list: int to print
+ * Return: Length of the int in decimal
+ **/
+int handle_int(va_list list)
 {
-     int printed_chars = 0;
-     va_list args;
+	char *int_c;
+	int len;
 
-     va_start(args, format);
+	int_c = int_ascii(va_arg(list, int), 10);
 
-     while (*format)
-     {
-          if (*format == '%')
-          {
-               format++;           /* Move past '%' */
-               if (*format == 'c') /* Handle %c conversion specifier */
-               {
-                    int c = va_arg(args, int);
-                    printed_chars += _myputchar(c);
-               }
-               else if (*format == 's') /* Handle %s conversion specifier */
-               {
-                    char *s = va_arg(args, char *);
-                    if (s == NULL)
-                         s = "(null)"; /* Print (null) for NULL strings */
-                    while (*s)
-                    {
-                         printed_chars += _myputchar(*s);
-                         s++;
-                    }
-               }
-               else if (*format == 'd' || *format == 'i') /* Handle %d and %i conversion specifiers */
-               {
-                    int num = va_arg(args, int);
+	len = print((int_c != NULL) ? int_c : "NULL");
 
-                    /* Handle negative numbers */
-                    int divisor = 1; /* Declare divisor here */
-                    if (num < 0)
-                    {
-                         _myputchar('-');
-                         num = -num;
-                    }
-
-                    /* Handle the digits of the number */
-                    while (num / divisor > 9)
-                    {
-                         divisor *= 10;
-                    }
-
-                    while (divisor > 0)
-                    {
-                         _myputchar('0' + (num / divisor));
-                         num %= divisor;
-                         divisor /= 10;
-                    }
-
-                    printed_chars += _myputchar('%');
-               }
-               else
-               {
-                    /* Print the character as is if not a known specifier */
-                    printed_chars += _myputchar('%');
-                    printed_chars += _myputchar(*format);
-               }
-          }
-          else
-          {
-               printed_chars += _myputchar(*format);
-          }
-
-          format++;
-     }
-
-     va_end(args);
-     return printed_chars;
-}
-
-/* Helper function to print a character */
-int _myputchar(char c)
-{
-     write(1, &c, 1);
-     return 1;
-}
-
-int main(void)
-{
-     /* Your code logic for testing _printf function */
-     return (0);
+	return (len);
 }
