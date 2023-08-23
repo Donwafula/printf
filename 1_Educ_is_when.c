497157eea432b4bd52ad4aebe9ce7f5ecc64ea73
donwafula <107577767+Donwafula@users.noreply.github.com>
2023-08-23 05:41:07 +0300

    Update 1_Educ_is_when.c

--- 1_Educ_is_when.c
+++ 1_Educ_is_when.c
@@ -16,3 +16,19 @@ int handle_int(va_list list)
 
 	return (len);
 }
+/**
+ * handle_oct - prints unsigned octal
+ * @list: num to print
+ * Return: len of num
+ */
+int handle_oct(va_list list)
+{
+	char *c;
+	int len;
+
+	c = int_ascii(va_arg(list, unsigned int), 8);
+
+	len = print((c != NULL) ? c : "NULL");
+
+	return (len);
+}
